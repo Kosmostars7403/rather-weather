8fecffc51e75e4147e3eb5d8683ce529b2cc5b01
Kosmostars7403 <64686091+Kosmostars7403@users.noreply.github.com>
2022-06-03 10:45:00 +0300

    Add type definition for current day

--- src/app/shared/services/weather.service.ts
+++ src/app/shared/services/weather.service.ts
@@ -1,6 +1,8 @@
 import {Inject, Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {API_URL} from "src/app/shared/providers/api-url.provider";
+import {CurrentDay} from "src/app/shared/models/current-day";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -13,8 +15,8 @@ export class WeatherService {
     @Inject(API_URL) private apiUrl: string
   ) {}
 
-  fetchCurrentWeather() {
-    return this.http.get(
+  fetchCurrentWeather(): Observable<CurrentDay> {
+    return this.http.get<CurrentDay>(
       `${this.apiUrl}weather`,
       {params: {
           lat: 59.9658185,
